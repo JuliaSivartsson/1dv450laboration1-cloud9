bf328e2c3d3089aa382215f383cb3766785486fb
JuliaSivartsson <jsigc09@student.lnu.se>
2016-01-21 14:05:42 +0000

    First commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(params[:user])
+    if(@user.save)
+      flash[:notice] = "Yay, your registration was successful"
+      flash[:color] = "valid"
+    else
+      flash[:notice] = "Darn, something went wrong!"
+      flash[:color] = "invalid"
+    end
+    render "new"
+  end
+end
